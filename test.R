ac66ce4080c644e09594830ffbb6c86863a82233
Max Lindmark <maxo.lindmark@gmail.com>
2019-04-03 14:18:20 +0200

    fig v2

--- test.R
+++ test.R
@@ -51,13 +51,16 @@ unique(dat$Sjö)
 
 pal <- c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#E69F00", "#D55E00")
 
+# This palette is used to make the line white for the rec fisheries. I use the other to get the basic palette in the legend though (most likely). This palette must give "white" in the right order, i.e. the order so that it matches rec fish in the data!
+pal_line <- c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#E69F00", "white")
+
 ## Now we need to tell ggplot to plot to use the order of the data and not the alphabetical order
 
 unique(dat$Sjö)
 
 dat$Sjö <- as.factor(dat$Sjö)
 
-dat$Sjö <- factor(dat$Sjö,
+dat$Sjö <- factor(dat$Sjö, # rename here, don't call it Sjö
                   levels = c("Stora sjöarna", 
                              "Vänern", 
                              "Vättern", 
@@ -65,20 +68,26 @@ dat$Sjö <- factor(dat$Sjö,
                              "Hjälmaren", 
                              "Fritidsfiske"))
 
+#======== A. MOST COMPLEX PLOT ========
+
 p <- ggplot(dat, aes(År, Ton, color = Sjö)) +
-   geom_bar(data = subset(dat, Sjö == "Stora sjöarna"), 
+  geom_bar(data = subset(dat, Sjö == "Stora sjöarna"), 
             aes(x = År, y = Ton), stat = "identity", color = pal[1], fill = pal[1], 
             width = 0.6) +
   geom_line(size = 1) +
-  geom_errorbar(data = dat, aes(x = År, ymin = rec_minu, ymax = rec_plus), 
-                show.legend = FALSE, width  = 1) +
-  scale_color_manual(values = pal) +
-  
+  geom_point(data = subset(dat, Sjö == "Fritidsfiske"), 
+             aes(År, Ton, fill = Sjö), size = 2, color = pal[6]) +
+  geom_errorbar(data = subset(dat, Sjö == "Fritidsfiske"), 
+                aes(x = År, ymin = rec_minu, ymax = rec_plus), 
+                show.legend = FALSE, width  = 1, color = pal[6]) +
+  scale_color_manual(values = pal_line) +
   labs(x = "", y = "Landningar (ton)") +
   ggtitle("Landningar") +
-  guides(color = guide_legend(nrow = 3, 
+  guides(fill = FALSE,
+         color = guide_legend(nrow = 3, 
                               title = "",
-                              override.aes = list(size = 1.3),
+                              override.aes = list(size = 1.3, 
+                                                  color = pal_line), # set pal if you want color in legend for Fritidsfiske, but then it's a line!
                               keywidth = 0.3,
                               keyheight = 0.1,
                               default.unit = "inch")) +
@@ -106,10 +115,13 @@ p <- ggplot(dat, aes(År, Ton, color = Sjö)) +
 
 p
 
-ggsave("fig_test.tiff", plot = p, dpi = 300, width = 8, height = 8, units = "cm")
+ggsave("fig_full.tiff", plot = p, dpi = 300, width = 8, height = 8, units = "cm")
 
 
-#### TEST 2 ####
+#======== B. SIMPLE PLOT FROM FULL (COMPLEX) CODE ========
+# code from old complex plot, need to update to make sure it's still playing with the 
+# modified version of the complex plot (i.e. the palette hacks)
+
 p2 <- ggplot(dat, aes(År, Ton)) +
   geom_bar(data = subset(dat, Sjö == "Stora sjöarna"), 
            aes(x = År, y = Ton, fill = Sjö), stat = "identity", 
@@ -117,16 +129,16 @@ p2 <- ggplot(dat, aes(År, Ton)) +
   #geom_line(size = 1) +
   #geom_errorbar(data = dat, aes(x = År, ymin = rec_minu, ymax = rec_plus), 
   #              show.legend = FALSE, width  = 1) +
-  scale_color_manual(values = pal) +
+  scale_fill_manual(values = pal) +
   
   labs(x = "", y = "Landningar (ton)") +
   ggtitle("Landningar") +
   guides(fill = guide_legend(nrow = 3, 
-                               title = "",
-                               override.aes = list(size = 1.3),
-                               keywidth = 0.3,
-                               keyheight = 0.1,
-                               default.unit = "inch")) +
+                             title = "",
+                             override.aes = list(size = 1.3),
+                             keywidth = 0.3,
+                             keyheight = 0.1,
+                             default.unit = "inch")) +
   scale_x_continuous(expand = c(0, 0), breaks = scales::pretty_breaks(n = 6)) +
   scale_y_continuous(expand = c(0, 0), breaks = scales::pretty_breaks(n = 5)) +
   theme_bw(base_size = 12) +
@@ -151,23 +163,9 @@ p2 <- ggplot(dat, aes(År, Ton)) +
 
 p2
 
-ggsave("fig_test2.tiff", plot = p2, dpi = 300, width = 8, height = 8, units = "cm")
+ggsave("fig_small.tiff", plot = p2, dpi = 300, width = 8, height = 8, units = "cm")
 
 
-
-#### TO DO ####
-# axis tick labels (year and tonnes ? Seems buggy, what is it that they want, a fixed number of ticks, from min to max? 
-
-
+#======== C. TO DO ========
+# axis tick labels (year and tonnes ? Seems buggy, what is it that they want, a fixed number of ticks, from min to max?  Wait until they reply about that
 # axis width (currently 0.2) 
-
-## what differs and should probably still difer?
-# note that I now have lines and bars for the main. That makes the life much easier, because the way ggplot (and thus R) automates things is that things are grouped. Otherwise we anyway have to do each species manually. But note tough even some minor manual things may be OK, because its an improvement from current one where everything is manual.
-
-# see point about lines and bars. The same argument is for the Fritidsfiske. Why a point? Point, like bars, are fine, as long as they also can have a line! This is for the legend, and otherwise all species would need a unique script, and we'd have a lot of un-linked scripts floating around with unique tweaks
-
-# See what the plot looks like when I don't have that many levels. Do margins look ok?
-
-
-
-
